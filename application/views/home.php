ce0bd6956d93b4a1ba17a099ddf596c21432ed07
Agung Priyo Sembodo <45793360+priyo000@users.noreply.github.com>
2019-04-22 07:14:35 +0700

    membuat Home Koperasi

---
+++ application/views/home.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="<?= base_url();?>icon.png" />
+
+
+  <title>Aplikasi Koperasi PeTIK</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="<?= base_url();?>vendor_asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body style="padding-top: 56px;">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="<?= base_url();?>">
+        <img src="<?= base_url();?>icon.png" style="width: 35px; height: 35px;">
+        Aplikasi Koperasi PeTIK
+      </a>
+      
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4 py-3">
+          <h1>Input Data Koperasi</h1><br>        
+      <div class="row text-center">
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <a href="#" class="btn btn-info">Transaksi</a>
+        </div>
+      </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <a href="#" class="btn btn-info">Investasi</a>
+        </div>
+      </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <a href="#" class="btn btn-info">Peminjaman</a>
+        </div>
+      </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <a href="<?= base_url()?>belanja" class="btn btn-info">Belanja</a>
+        </div>
+      </div>
+        </div>
+    </div>
+    </header>
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4 py-3">
+          <h1>Daftar Riwayat Koperasi</h1><br>
+
+      <div class="row text-center">
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Transaksi</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Investasi</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Peminjaman</a>
+            </div>
+          </div>
+        </div>
+
+    </div>
+
+    <div class="row text-center">
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Belanja</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Anggota</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <a href="#" class="btn btn-info">Produk</a>
+            </div>
+          </div>
+        </div>
+
+    </div>
+    </header>
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Pesantren Teknologi Informasi dan Komunikasi 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?= base_url();?>vendor_asset/jquery/jquery.min.js"></script>
+  <script src="<?= base_url();?>vendor_asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
